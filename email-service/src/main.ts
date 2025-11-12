809c21f082b58ca9f9b63b672169ee669e072cdb
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2025-11-12 12:40:02 +0100

    feat: enhance bootstrap function to setup RabbitMQ and connect microservice

--- email-service/src/main.ts
+++ email-service/src/main.ts
@@ -1,8 +1,44 @@
+// src/main.ts (Corrected Function)
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { RabbitMQSetupService } from './common/services/rabbitmq-setup.service';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const appContext = await NestFactory.createApplicationContext(AppModule);
+
+  const setupService = appContext.get(RabbitMQSetupService);
+
+  const configService = appContext.get(ConfigService);
+  const rabbitConfig = configService.get('rabbit');
+
+  await setupService.setupQueuesAndExchanges();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [rabbitConfig.url],
+      queue: rabbitConfig.emailQueue,
+      queueOptions: {
+        durable: true,
+        deadLetterExchange: rabbitConfig.dlxExchange,
+        deadLetterRoutingKey: 'retry.email.1',
+        assertQueue: false,
+      },
+      noAck: false,
+    },
+  });
+
+  await app.startAllMicroservices();
+
+  await app.listen(3000, () => {
+    console.log(
+      `Application running and Microservice listener started on queue: ${rabbitConfig.emailQueue}`,
+    );
+  });
 }
 bootstrap();
