5787365e3c1f0c1a6f4b391c83f8e5052bf383ab
Abisoye Ogunmona <Ogunmonaa@gmail.com>
2025-11-12 08:05:06 +0100

    feat(gateway): adds notification service dependencies and improves lint rules
    - Introduces package files for a new notification service with NestJS microservices and AMQP support.
    
    - Enhances gateway lint rules for better TypeScript safety and adjusts configuration for consistency.
    
    - Removes unused code and adds HTTP module import for improved user service integration.

--- gateway/src/db/data-source.options.ts
+++ gateway/src/db/data-source.options.ts
@@ -1,16 +1,3 @@
-// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-
-// export const dataSourceOptions: TypeOrmModuleOptions = {
-//   type: 'postgres',
-//   host: process.env.DB_HOST,
-//   port: 5432,
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME,
-//   autoLoadEntities: true,
-//   synchronize: true,
-// };
-
 import { DataSourceOptions } from 'typeorm';
 import * as path from 'path';
 
